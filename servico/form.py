963d3188e992551e2a7f56b59538bd69c0c0d1f9
thonnycleuton <thonnycleuton@gmail.com>
2017-07-30 21:16:21 -0300

    Formularios para Clientes

--- servico/form.py
+++ servico/form.py
@@ -6,10 +6,11 @@ class ServicoForm(forms.ModelForm):
 
     class Meta:
         model = Servico
-        fields = ['nome', 'descricao', 'valor', 'foto', 'status_ativo']
+        fields = ['nome', 'descricao', 'valor', 'status_ativo', 'foto', ]
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control'}),
-            'descricao': forms.Textarea(attrs={'class': 'form-control'}),
+            'descricao': forms.TextInput(attrs={'class': 'form-control'}),
             'valor': forms.NumberInput(attrs={'class': 'form-control'}),
-            'status_ativo': forms.CheckboxInput(attrs={'class': 'icheckbox'})
+            'status_ativo': forms.CheckboxInput(attrs={'class': 'icheckbox'}),
+            'foto': forms.FileInput(attrs={'class': 'form-control'}),
         }
--- servico/views.py
+++ servico/views.py
@@ -14,10 +14,10 @@ class ServicoList(ListView):
     context_object_name = 'servico_list'
 
 
-class ServicoCreate(FormView):
+class ServicoCreate(CreateView):
 
+    model = Servico
     form_class = ServicoForm
-    template_name = 'servico/servico_form.html'
     success_url = reverse_lazy('servicos:list')
 
     def form_valid(self, form):
@@ -29,8 +29,14 @@ class ServicoCreate(FormView):
 
 class ServicoUpdate(UpdateView):
     model = Servico
-    success_url = reverse_lazy('servico_list')
-    fields = '__all__'
+    form_class = ServicoForm
+    success_url = reverse_lazy('servicos:list')
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super(ServicoUpdate, self).form_valid(form)
 
 
 class ServicoDelete(DeleteView):

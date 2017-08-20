1172f19d1f87be786c4b22a5e51859d62f5fa084
thonnycleuton <thonnycleuton@gmail.com>
2017-08-20 16:43:08 -0300

    Sistema de busca indexada com Haystack e ElastickSearch

--- search/search_indexes.py
+++ search/search_indexes.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.models import User
 from haystack import indexes
 from accounts.models import Profile
 
 
 class UserIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.EdgeNgramField(document=True, use_template=True)
+    text = indexes.CharField(document=True, use_template=True)
     username = indexes.CharField(model_attr='username')
+    content_auto = indexes.EdgeNgramField(model_attr='username')
 
     def get_model(self):
-        return Profile
+        return User
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""

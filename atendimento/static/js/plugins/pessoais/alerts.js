6da4373660084411da3a59ec88827a5f6a4042d3
thonnycleuton <thonnycleuton@gmail.com>
2017-08-21 21:01:25 -0300

    Listagens em geral

--- atendimento/static/js/plugins/pessoais/alerts.js
+++ atendimento/static/js/plugins/pessoais/alerts.js
@@ -1,56 +1,42 @@
 $(function () {
 
-  /* Functions */
+	/* Functions */
 
-  var loadForm = function () {
-    var btn = $(this);
-    $.ajax({
-      url: btn.attr("data-url"),
-      type: 'get',
-      dataType: 'json',
-      beforeSend: function () {
-        $("#modal-book .modal-content").html("");
-        $("#modal-book").modal("show");
-      },
-      success: function (data) {
-        $("#modal-book .modal-content").html(data.html_form);
-      }
-    });
-  };
+	var loadForm = function () {
+		var btn = $(this);
+		$.ajax({
+			url: btn.attr("data-url"),
+			type: 'get',
+			dataType: 'json',
+			beforeSend: function () {
+				$("#message-box-warning").modal("show");
+			},
+			success: function (data) {
+				$("#message-box-warning .mb-container").html(data.html_form);
+			}
+		});
+	};
 
-  var saveForm = function () {
-    var form = $(this);
-    $.ajax({
-      url: form.attr("action"),
-      data: form.serialize(),
-      type: form.attr("method"),
-      dataType: 'json',
-      success: function (data) {
-        if (data.form_is_valid) {
-          $("#book-table tbody").html(data.html_book_list);
-          $("#modal-book").modal("hide");
-        }
-        else {
-          $("#modal-book .modal-content").html(data.html_form);
-        }
-      }
-    });
-    return false;
-  };
+	var saveForm = function () {
+		var form = $(this);
+		$.ajax({
+			url: form.attr("action"),
+			data: form.serialize(),
+			type: form.attr("method"),
+			dataType: 'json',
+			success: function (data) {
+				if (data.form_is_valid) {
+					$("#tabela tbody").html(data.html_book_list);
+					$("#message-box-warning").modal("hide");
+				} else {
+					$("#message-box-warning .mb-container").html(data.html_form);
+				}
+			}
+		});
+		return false;
+	};
 
-
-  /* Binding */
-
-  // Create book
-  $(".js-create-book").click(loadForm);
-  $("#modal-book").on("submit", ".js-book-create-form", saveForm);
-
-  // Update book
-  $("#book-table").on("click", ".js-update-book", loadForm);
-  $("#modal-book").on("submit", ".js-book-update-form", saveForm);
-
-  // Delete book
-  $("#book-table").on("click", ".js-delete-book", loadForm);
-  $("#modal-book").on("submit", ".js-book-delete-form", saveForm);
-
+	// Delete book
+	$("#tabela").on("click", ".js-delete-book", loadForm);
+	$("#message-box-warning").on("submit", ".js-book-delete-form", saveForm);
 });

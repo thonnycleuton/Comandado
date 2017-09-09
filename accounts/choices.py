0e400daa518eddc7f176b4edc18834027f70c393
thonnycleuton <thonnycleuton@gmail.com>
2017-09-09 12:56:55 -0300

    Regras para diferentes usuarios

---
+++ accounts/choices.py
@@ -0,0 +1,12 @@
+
+FUNCAO = (
+    (1, "Cabelereira"),
+    (2, "Depiladora"),
+    (3, "Esteticista"),
+    (4, "Gerente"),
+    (5, "Manicure"),
+    (6, "Massoterapeuta"),
+    (7, "Nutrionista"),
+    (8, "Persona Trainer"),
+    (9, "Recepcionista"),
+)
